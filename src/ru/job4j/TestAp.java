3b53c85e605dfa5a245a1ef788dc047308469e59
alexsl809 <merchant2210@gmail.com>
2020-04-15 00:10:42 +0400

    0. Что такое тестирование.[#272861]

---
+++ src/ru/job4j/TestAp.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+import java.util.*;
+public class TestAp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+
+        int number = sc.nextInt();
+
+        System.out.println("Спасибо! Вы ввели число " + number);
+
+    }
+}
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -17,5 +17,18 @@ public class Converter {
         int dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollar.");
+
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        int in1 = 140;
+        int expected1 = 2;
+        int out1 = rubleToDollar(in);
+        boolean passed1 = expected1 == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
     }
+
 }
