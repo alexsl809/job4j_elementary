abf822d9b3811c730f868014feebb67b62f28a89
alexsl809 <merchant2210@gmail.com>
2020-04-14 19:41:58 +0400

    5. Конвертер валюты[#272826]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -2,19 +2,32 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int onePlusTwo = one + two;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+       Calculator.plus(1, 2);
+       Calculator.plus(10, 11);
+    }
+}
+class MathFunc {
+
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+
+    public static int func2(int x) {
+        int y = 1 / x;
+        return y;
+    }
+
+    public static void main(String[] args) {
+        int result1 = MathFunc.func1(3);
+        int result2 = MathFunc.func1(5);
+        int total = result1 + result2;
+        System.out.println(total);
     }
 }
